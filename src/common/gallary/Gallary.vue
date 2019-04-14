7a72e850858fc7125717568c94d3a7c4295848e8
star <startjcu@163.com>
2019-04-14 08:49:15 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item,index) of imgs" :key="index">
+          <img class="gallary-img" :src="item">
+        </swiper-slide>
+        <swiper-slide>
+          <img
+            class="gallary-img"
+            src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg"
+          >
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods:{
+    handleGallaryClick(){
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-.container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      overflow: hidden
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        class="banner-img"
+        src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"
+        alt
+      >
+      <div class="banner-info">
+        <div class="banner-title">故宫(AAAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>39故宫
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data() {
+    return {
+      showGallary: false,
+      imgs: []
+    }
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallary = true
+    },
+    handleGallaryClose() {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute 
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding:0 .2rem
+      .banner-number
+        padding: 0 .4rem
+        height: .32rem
+        margin-top: .18rem
+        line-height: .32rem
+        border-rudius: .2rem
+        background: rgba(0, 0, 0, .4)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
